23d6c9d8e4f8c057b726aa1e46ef573602e41274
Anand Balakrishnan <anandbala1597@gmail.com>
2019-11-12 18:02:51 -0800

    Add fish shell

---
+++ .config/fish/config.fish
@@ -0,0 +1,22 @@
+# -----------------------------------------------------------------------------
+# Setup PATH from scratch
+# -----------------------------------------------------------------------------
+
+# Setup macOS specific paths
+if test (uname) = "Darwin"
+  set -x PATH $PATH /opt/X11/bin /Library/TeX/texbin
+end
+
+# Common directories to add, if they exist
+for p in "$HOME/bin" "$HOME/.local/bin" "/snap/bin"
+  if test -d $p
+    set -x PATH $p $PATH
+  end
+end
+
+if type -q nvim
+  set -gx EDITOR nvim
+else
+  set -gx EDITOR vim
+end
+
