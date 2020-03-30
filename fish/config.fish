2d1f239bfa3658e6bf4b6ba1f8803e4a4e1905dc
Anand Balakrishnan <anandbala1597@gmail.com>
2020-03-29 22:56:16 -0700

    Minor stuff

--- fish/config.fish
+++ fish/config.fish
@@ -22,11 +22,13 @@ end
 
 # --- FZF config
 if test -e $HOME/.fzf/bin/fzf
-  set -gx   FZF_FIND_FILE_COMMAND       "fd -L -t f -t l -t x . \$dir"
+  set -gx   FZF_LEGACY_KEYBINDINGS 0
+
+  set -gx   FZF_FIND_FILE_COMMAND       "fd -L -t f . \$dir"
   set -gx   FZF_CD_COMMAND              "fd -L -t d . \$dir"
   set -gx   FZF_CD_WITH_HIDDEN_COMMAND  "fd -H -L -t d . \$dir"
 
-  set -gx   FZF_DEFAULT_COMMAND         "fd -L ."
+  set -gx   FZF_DEFAULT_COMMAND         "fd -L -t f ."
   set -gx   FZF_CTRL_T_COMMAND          "$FZF_DEFAULT_COMMAND"
   set -gx   FZF_ALT_C_COMMAND           "fd -L -t d ."
 
