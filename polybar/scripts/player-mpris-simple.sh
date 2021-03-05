8aafbe14831b7b2185969ae8b1e23020a3b7c1d6
Anand Balakrishnan <anandbala1597@gmail.com>
2021-03-05 14:43:17 -0800

    [polybar] Minor icon change for music

--- polybar/scripts/player-mpris-simple.sh
+++ polybar/scripts/player-mpris-simple.sh
@@ -2,9 +2,11 @@
 
 player_status=$(playerctl status 2> /dev/null)
 
-playing=false
-if [ "$player_status" = "Playing" ] || [ "$player_status" = "Paused" ]; then
-  playing=true
+playing=""
+if [ "$player_status" = "Playing" ]; then
+playing=""
+elif [ "$player_status" = "Paused" ]; then
+playing=""
 else
   exit 0
 fi
@@ -12,4 +14,4 @@ fi
 artist=$(playerctl metadata artist | sed -e 's/\(.\{25\}\).*$/\1.../')
 title=$(playerctl metadata title | sed -e 's/\(.\{25\}\).*$/\1.../')
 
-echo " $artist - $title"
+echo " $playing $artist - $title"
