6423efcb05359d066852e087a8535e82cafc9550
Anand Balakrishnan <anandbala1597@gmail.com>
2021-03-26 11:21:48 -0700

    [polybar] Exclude firefox from MPRIS players

--- polybar/scripts/player-mpris-simple.sh
+++ polybar/scripts/player-mpris-simple.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-player_status=$(playerctl status 2> /dev/null)
+# Comma-separated list of applications that should be excluded by playerctl
+excluded_players="firefox"
+
+player_status=$(playerctl -i "$excluded_players" status 2> /dev/null)
 
 playing=""
 if [ "$player_status" = "Playing" ]; then
@@ -8,10 +11,11 @@ playing=""
 elif [ "$player_status" = "Paused" ]; then
 playing=""
 else
+  echo ""
   exit 0
 fi
 
-artist=$(playerctl metadata artist | sed -e 's/\(.\{25\}\).*$/\1.../')
-title=$(playerctl metadata title | sed -e 's/\(.\{25\}\).*$/\1.../')
+artist=$(playerctl -i "$excluded_players" metadata artist | sed -e 's/\(.\{25\}\).*$/\1.../')
+title=$(playerctl -i "$excluded_players" metadata title | sed -e 's/\(.\{25\}\).*$/\1.../')
 
 echo " $playing $artist - $title"
