1a984d5e53c3cd4e19bb2b1d6b59a9026df97d97
Anand Balakrishnan <anandbala1597@gmail.com>
2021-04-01 20:25:22 -0700

    [fish][carla] Use find instead of fd

--- fish/plugins/carla_paths/conf.d/carla_paths.fish
+++ fish/plugins/carla_paths/conf.d/carla_paths.fish
@@ -1,11 +1,9 @@
-# Assumes the `fd` is installed in your system because `find` kinda sucks
-
 # List of paths to search for a carla directory (at the top level)
 set -l carla_search_paths /opt
 
-# Use `fd` to search for a top level directory that has the name `carla` in it, e.g.,
+# Search for a top level directory that has the name `carla` in it, e.g.,
 # /opt/carla-simulator
-set -l carla_root (fd -t d -d 1 carla . $carla_search_paths | head -n1)
+set -l carla_root (find $carla_search_paths -maxdepth 1 -type d -name 'carla*' | head -n1)
 
 # Path to the Pythonroot path for the python API
 set -l carla_pyroot "$carla_root/PythonAPI"
@@ -13,7 +11,7 @@ set -l carla_pyroot "$carla_root/PythonAPI"
 # if both carla_root and carla_pyroot exist
 if test -d $carla_root -a -d $carla_pyroot
   # find the egg file for python3
-  set -l carla_pyegg (fd -t f -e egg py3 $carla_root)
+  set -l carla_pyegg (find $carla_root -type f \( -name "*.egg" -a -name "*py3*" \) | head -n1)
   # set the CARLA_ROOT
   set -gx CARLA_ROOT $carla_root
   # set the PYTHONPATH
