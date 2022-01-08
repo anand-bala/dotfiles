9e34b31a60d16139c75b37e4c316274b970851e2
Anand Balakrishnan <anandbala1597@gmail.com>
2022-01-07 20:26:38 -0800

    [nvim][python] better folding

--- nvim/after/ftplugin/python.lua
+++ nvim/after/ftplugin/python.lua
@@ -6,3 +6,12 @@ vim.opt_local.textwidth = 88
 if vim.fn.exists ":VTerm" then
   vim.cmd [[command! -buffer -nargs=0 Repl VTerm ipython]]
 end
+
+vim.treesitter.set_query(
+  "python",
+  "folds",
+  [[
+  (function_definition (block) @fold)
+  (class_definition (block) @fold)
+]]
+)
