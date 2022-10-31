099c12409e1ca02e2d8d5e47923bc5e5cfbc4c4b
Anand Balakrishnan <anandbala1597@gmail.com>
2022-10-31 16:22:12 -0700

    [nvim] update c/c++ doc comments

--- nvim/after/ftplugin/c.lua
+++ nvim/after/ftplugin/c.lua
@@ -1,5 +1,5 @@
 -- Add triple-slash comments
-vim.opt_local.comments:prepend { ":///" }
+vim.opt_local.comments:prepend { ":///", "://!" }
 
 -- Usual clang-format options I set for my projects
 vim.opt_local.textwidth = 88
--- nvim/after/ftplugin/cpp.lua
+++ nvim/after/ftplugin/cpp.lua
@@ -1,5 +1,5 @@
 -- Add triple-slash comments
-vim.opt_local.comments:prepend { ":///" }
+vim.opt_local.comments:prepend { ":///", "://!" }
 vim.opt_local.matchpairs:append { "<:>" }
 
 -- Usual clang-format options I set for my projects
