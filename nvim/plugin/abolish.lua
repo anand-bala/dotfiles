052960074956c88e6dbc002dbfa952bde30e4929
Anand Balakrishnan <anandbala1597@gmail.com>
2022-10-31 16:24:32 -0700

    [nvim] update abolish list

--- nvim/plugin/abolish.lua
+++ nvim/plugin/abolish.lua
@@ -15,8 +15,14 @@ do
     callback = function()
       abolish { "desparat{e,es,ed,ing,ely,ion,ions,or}", "desperat{}" }
       abolish { "seperat{e,es,ed,ing,ely,ion,ions,or}", "separat{}" }
-      abolish { "recieve", "receive" }
-      abolish { "beleive", "believe" }
+      abolish { "reciev{e,es,ed,ing}", "receiv{}" }
+      abolish { "beleiv{e,es,ed,ing}", "believ{}" }
+      abolish { "cal{a,e}nder{,s}", "cal{e}ndar{}" }
+      abolish { "{,non}existan{ce,t}", "{}existen{}" }
+      abolish { "{,un}nec{ce,ces,e}sar{y,ily}", "{}nec{es}sar{}" }
+      abolish { "reproducable", "reproducible" }
+      abolish { "rec{co,com,o}mend{,s,ed,ing,ation}", "rec{om}mend{}" }
+      abolish { "{,ir}releven{ce,cy,t,tly}", "{}relevan{}" }
     end,
   })
 end
