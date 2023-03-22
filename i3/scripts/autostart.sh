61fb1dac3ec98fb1ee0943905488d1e237ddb241
Anand Balakrishnan <anandbala1597@gmail.com>
2023-03-22 11:41:39 -0700

    [i3] use polybar instead of tint2

--- i3/scripts/autostart.sh
+++ i3/scripts/autostart.sh
@@ -1,14 +1,32 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
-
 config_home="${XDG_CONFIG_HOME:-$HOME/.config}"
 script_dir=$config_home/i3/scripts
 
-# Autostart XDG-based applications
-if command -v dex &> /dev/null; then
-  dex -a -e i3wm -s $config_home/autostart
-elif command -v dex-autostart &> /dev/null; then
-  dex-autostart -a -e i3wm -s $config_home/autostart
+if command -v nitrogen &> /dev/null; then
+  nitrogen --restore
 fi
 
+if command -v polybar &> /dev/null; then
+  # Terminate already running bar instances
+  # If all your bars have ipc enabled, you can use 
+  polybar-msg cmd quit
+  # Otherwise you can use the nuclear option:
+  # killall -q polybar
+
+  # Launch
+  echo "---" | tee -a ~/.cache/polybar.log
+  polybar 2>&1 | tee -a ~/.cache/polybar.log & disown
+fi
+
+if command -v pasystray &> /dev/null; then
+  killall pasystray
+  pasystray &> /dev/null
+fi
+
+# Autostart XDG-based applications
+if command -v dex &> /dev/null; then
+  dex -a -s $config_home/autostart
+elif command -v dex-autostart &> /dev/null; then
+  dex-autostart -a -s $config_home/autostart
+fi
