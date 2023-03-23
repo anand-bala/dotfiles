10c5f26fc963526577a46b36d116a8aff3fdd5e4
Anand Balakrishnan <anandbala1597@gmail.com>
2023-03-23 12:21:07 -0700

    [i3] launch polybar correctly from i3config

--- i3/scripts/autostart.sh
+++ i3/scripts/autostart.sh
@@ -7,21 +7,23 @@ if command -v nitrogen &> /dev/null; then
   nitrogen --restore
 fi
 
-if command -v polybar &> /dev/null; then
-  # Terminate already running bar instances
-  # If all your bars have ipc enabled, you can use 
-  polybar-msg cmd quit
-  # Otherwise you can use the nuclear option:
-  # killall -q polybar
-
-  # Launch
-  echo "---" | tee -a ~/.cache/polybar.log
-  polybar 2>&1 | tee -a ~/.cache/polybar.log & disown
-fi
+# if command -v polybar &> /dev/null; then
+#   # Terminate already running bar instances
+#   # If all your bars have ipc enabled, you can use 
+#   polybar-msg cmd quit
+#   # Otherwise you can use the nuclear option:
+#   # killall -q polybar
+#
+#   # Launch
+#   echo "---" | tee -a ~/.cache/polybar.log
+#   polybar 2>&1 | tee -a ~/.cache/polybar.log &
+#   disown $!
+# fi
 
 if command -v pasystray &> /dev/null; then
   killall pasystray
   pasystray &> /dev/null
+  disown $!
 fi
 
 # Autostart XDG-based applications
