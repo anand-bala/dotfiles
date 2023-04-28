1f8a5729a34b0039efe6b5e9c85abacbc2a11d34
Anand Balakrishnan <anandbala1597@gmail.com>
2023-04-28 16:33:36 -0700

    [nvim] distable LSP-based formatexpr for md, tex

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -1,1 +1,12 @@
-vim.opt_local.formatexpr = ""
+local autocmd = vim.api.nvim_create_autocmd
+
+vim.opt_local.tabstop = 2     -- Size of a hard tab (which will be expanded)
+vim.opt_local.softtabstop = 2 -- Size of a soft tab
+vim.opt_local.shiftwidth = 2  -- Size of indent
+
+autocmd("LspAttach", {
+  callback = function(args)
+    -- Use builtin formatexpr for Markdown and Tex
+    vim.bo[args.buf].formatexpr = nil
+  end,
+})
--- nvim/after/ftplugin/tex.lua
+++ nvim/after/ftplugin/tex.lua
@@ -1,1 +1,10 @@
+local autocmd = vim.api.nvim_create_autocmd
+
 vim.opt.textwidth = 80
+
+autocmd("LspAttach", {
+  callback = function(args)
+    -- Use builtin formatexpr for Markdown and Tex
+    vim.bo[args.buf].formatexpr = nil
+  end,
+})
