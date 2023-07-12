a43eac190277003f681e6610fe4258ab640e2dfa
Anand Balakrishnan <anandbala1597@gmail.com>
2023-07-12 10:04:26 -0400

    fix(nvim): refresh mason registry before installing

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -6,18 +6,23 @@ local mason = {
   opts = {
     ensure_installed = {
       "stylua",
-      -- "commitlint",
+      "lua-language-server",
+      "shellcheck",
+      "shfmt",
+      "shellharden",
     },
   },
   config = function(_, opts)
     require("mason").setup(opts)
     local mr = require "mason-registry"
-    for _, tool in ipairs(opts.ensure_installed) do
-      local p = mr.get_package(tool)
-      if not p:is_installed() then
-        p:install()
+    mr.refresh(function()
+      for _, tool in ipairs(opts.ensure_installed) do
+        local p = mr.get_package(tool)
+        if not p:is_installed() then
+          p:install()
+        end
       end
-    end
+    end)
   end,
 }
 
