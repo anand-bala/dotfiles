aafb72dce174d8f98a9751b069aeec0bde8b96d6
Anand Balakrishnan <anandbala1597@gmail.com>
2023-07-28 11:46:12 -0700

    nvim: prefer some indent_blankline defaults

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -129,10 +129,8 @@ return {
     config = function()
       vim.g.indent_blankline_char = "┆"
       vim.g.indent_blankline_char_blankline = "┆"
-      vim.g.indent_blankline_buftype_exclude = { "terminal" }
       vim.g.indent_blankline_show_trailing_blankline_indent = false
       vim.g.indent_blankline_show_current_context = true
-      vim.g.indent_blankline_filetype_exclude = { "help", "terminal" }
       -- disabled now for performance hit.
       vim.g.indent_blankline_use_treesitter = true
       require("indent_blankline").setup {}
