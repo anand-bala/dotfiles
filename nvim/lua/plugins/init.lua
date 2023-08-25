ca6dcc82da1228200212a9756cb71753737f78f6
Anand Balakrishnan <anandbala1597@gmail.com>
2023-08-25 11:18:29 -0700

    move colorblindness explanation here

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,4 +1,16 @@
 --- Colorscheme
+---
+--- # My Colorblindness simulation
+---
+--- Source: https://daltonlens.org/evaluating-cvd-simulation/#Generating-Ishihara-like-plates-for-self-evaluation
+---
+--- | Plate method           | Protan | Deutan | Tritan |
+--- | ---------------------- | ------ | ------ | ------ |
+--- | Brettel 1997 sRGB      | 0.4    | 0.9    | 0.1    |
+--- | Vischeck (Brettel CRT) | 0.3    | 0.9    | 0.1    |
+--- | Viénot 1999 sRGB       | 0.4    | 0.9    | 0.1    |
+--- | Machado 2009 sRGB      | 0.4    | 0.8    | 0.1    |
+---
 ---@type LazyPluginSpec
 local colorscheme = {
   "EdenEast/nightfox.nvim",
