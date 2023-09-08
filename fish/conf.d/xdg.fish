aa8677eb4cd456b63cd626b5a6f2f24a85310f97
Anand Balakrishnan <anandbala1597@gmail.com>
2023-09-08 11:13:33 -0700

    fish: minor xdg change

--- fish/conf.d/xdg.fish
+++ fish/conf.d/xdg.fish
@@ -14,19 +14,19 @@ set -l cache_home   $HOME/.cache
 set -l state_home   $HOME/.local/state
 
 contains -- $data_home $XDG_DATA_HOME
-  or set -gx --path XDG_DATA_HOME $data_home $XDG_DATA_HOME
+  or set -gx --path XDG_DATA_HOME $data_home
 
 contains -- $data_home $XDG_DATA_DIRS
   or set -gx --path XDG_DATA_DIRS $data_home $XDG_DATA_DIRS
 
 contains -- $config_home $XDG_CONFIG_HOME
-  or set -gx --path XDG_CONFIG_HOME $config_home $XDG_CONFIG_HOME
+  or set -gx --path XDG_CONFIG_HOME $config_home
 
 contains -- $cache_home $XDG_CACHE_HOME
-  or set -gx --path XDG_CACHE_HOME $cache_home $XDG_CACHE_HOME
+  or set -gx --path XDG_CACHE_HOME $cache_home
 
 contains -- $state_home $XDG_STATE_HOME
-  or set -gx --path XDG_STATE_HOME $state_home $XDG_STATE_HOME
+  or set -gx --path XDG_STATE_HOME $state_home
 
 # --- Setup paths for flatpak
 if command -sq -- flatpak
