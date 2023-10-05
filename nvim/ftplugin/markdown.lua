fcb49a40a3cc21c472a1be9ae3861cb69dc51253
Anand Balakrishnan <anandbala1597@gmail.com>
2023-10-05 08:38:45 -0700

    nvim: configure markdown for quarto

--- nvim/ftplugin/markdown.lua
+++ nvim/ftplugin/markdown.lua
@@ -2,5 +2,8 @@ vim.opt_local.tabstop = 2 -- Size of a hard tab (which will be expanded)
 vim.opt_local.softtabstop = 2 -- Size of a soft tab
 vim.opt_local.shiftwidth = 2 -- Size of indent
 
+vim.opt_local.formatoptions = vim.opt_local.formatoptions + "]"
+vim.opt_local.formatlistpat = [[^\s*\(\d\+[\]:.)}\t ]\)\|\(\\item \)\s*]]
+
 -- Use builtin formatexpr for Markdown and Tex
 vim.opt_local.formatexpr = nil
