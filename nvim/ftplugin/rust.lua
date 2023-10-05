15e4f38a602586d629e9a4c16a97949a204f6969
Anand Balakrishnan <anandbala1597@gmail.com>
2023-10-05 08:39:07 -0700

    nvim: disable LSP inlay hints for rust

--- nvim/ftplugin/rust.lua
+++ nvim/ftplugin/rust.lua
@@ -1,1 +1,9 @@
+local onattach = require("config.lsp").on_attach_hook
+
 vim.opt.textwidth = 88
+
+onattach(function(client, buffer)
+  vim.lsp.inlay_hint(buffer, false)
+end, {
+  desc = "disable LSP inlay hints for Rust. Use rust-tools.",
+})
