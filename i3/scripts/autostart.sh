6a108903d28491624c56b62501112ca15a86c2e3
Anand Balakrishnan <anandbala1597@gmail.com>
2024-02-12 13:43:37 -0800

    (i3wm): do autostart things better

--- i3/scripts/autostart.sh
+++ i3/scripts/autostart.sh
@@ -3,32 +3,11 @@
 config_home="${XDG_CONFIG_HOME:-$HOME/.config}"
 script_dir=$config_home/i3/scripts
 
-if command -v nitrogen &> /dev/null; then
-  nitrogen --restore
-fi
-
-# if command -v polybar &> /dev/null; then
-#   # Terminate already running bar instances
-#   # If all your bars have ipc enabled, you can use 
-#   polybar-msg cmd quit
-#   # Otherwise you can use the nuclear option:
-#   # killall -q polybar
-#
-#   # Launch
-#   echo "---" | tee -a ~/.cache/polybar.log
-#   polybar 2>&1 | tee -a ~/.cache/polybar.log &
-#   disown $!
-# fi
-
-# if command -v pasystray &> /dev/null; then
-#   killall pasystray
-#   pasystray &> /dev/null
-#   disown $!
-# fi
+bash "$config_home/autorandr/postswitch.d/00-always-run"
 
 # Autostart XDG-based applications
-if command -v dex &> /dev/null; then
-  dex -a -s $config_home/autostart
-elif command -v dex-autostart &> /dev/null; then
-  dex-autostart -a -s $config_home/autostart
+if command -v dex &>/dev/null; then
+	dex -a -s "$config_home/autostart"
+elif command -v dex-autostart &>/dev/null; then
+	dex-autostart -a -s "$config_home/autostart"
 fi
