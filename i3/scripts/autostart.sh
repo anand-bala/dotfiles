177a5d1963db390f988c46fa21a9e04a95b2c956
Anand Balakrishnan <anandbala1597@gmail.com>
2025-12-02 15:18:22 -0600

    move i3wm autostart things into scripts

--- i3/scripts/autostart.sh
+++ i3/scripts/autostart.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
 config_home="${XDG_CONFIG_HOME:-$HOME/.config}"
-script_dir=$config_home/i3/scripts
-
-bash "$config_home/autorandr/postswitch.d/00-always-run"
 
 # Autostart XDG-based applications
-if command -v dex &>/dev/null; then
-	dex -a -s "$config_home/autostart"
-elif command -v dex-autostart &>/dev/null; then
-	dex-autostart -a -s "$config_home/autostart"
+if type dex >/dev/null; then
+  dex -a -s "$config_home/autostart"
+elif type dex-autostart >/dev/null; then
+  dex-autostart -a -s "$config_home/autostart"
 fi
