3310acf939f7b30918576c2f556fba378a388f87
Anand Balakrishnan <anandbala1597@gmail.com>
2025-12-03 23:30:10 -0600

    add sway configuration that works

---
+++ sway/bin/__rofi_poweroff_menu.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+if [[ $# -eq 0 ]]; then
+  echo -en "Lock\0icon\x1fsystem-lock-screen\n"
+  echo -en "Logout\0icon\x1fsystem-log-out\n"
+  echo -en "Suspend\0icon\x1fsystem-suspend\n"
+  # echo -en "Hibernate\0icon\x1fsystem-hibernate\n"
+  echo -en "Hybrid-sleep\0icon\x1fsystem-hibernate\n"
+  echo -en "Shutdown\0icon\x1fsystem-shutdown\n"
+  echo -en "Reboot\0icon\x1fsystem-reboot\n"
+else
+  if [ "$1" = "Shutdown" ]; then
+    systemctl poweroff
+  elif [ "$1" = "Logout" ]; then
+    loginctl terminate-session "$XDG_SESSION_ID"
+  elif [ "$1" = "Reboot" ]; then
+    systemctl reboot
+  elif [ "$1" = "Suspend" ]; then
+    systemctl suspend
+  elif [ "$1" = "Hibernate" ]; then
+    systemctl hibernate
+  elif [ "$1" = "Hybrid-sleep" ]; then
+    systemctl hybrid-sleep
+  elif [ "$1" = "Lock" ]; then
+    loginctl lock-session
+  fi
+fi
