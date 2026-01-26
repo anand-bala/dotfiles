81a64a84124e0a7bc180b087e8f42b31364a851a
Anand Balakrishnan <anandbala1597@gmail.com>
2026-01-25 20:58:38 -0600

    fish: simplify nvim launching (flatten is handled in nvim config)

--- fish-shell/dot-config/fish/config.fish
+++ fish-shell/dot-config/fish/config.fish
@@ -12,7 +12,7 @@ end
 
 if command -sq -- nvim 
   set -gx EDITOR nvim
-  set -gx VISUAL "nvim --cmd 'let g:flatten_wait=1'"
+  set -gx VISUAL nvim
   set -gx MANPAGER "nvim +Man!"
   alias e="nvim"
 else
